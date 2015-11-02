5891047e9816a58506afb9b7bc90e2468a33529f
mikepitre <michaelepitre@gmail.com>
2015-11-02 11:32:10 -0600

    added addPostVC and created segue

---
+++ DailyScenes/AddPostVC.swift
@@ -0,0 +1,19 @@
+//
+//  AddPostVC.swift
+//  DailyScenes
+//
+//  Created by Mike Pitre on 11/2/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
--- DailyScenes/ViewController.swift
+++ DailyScenes/ViewController.swift
@@ -19,6 +19,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        //test data
+        let post = Post(imagePath: "", title: "Post1", description: "Post 1 Description")
+        let post2 = Post(imagePath: "", title: "Post2", description: "Post 2 Description")
+        let post3 = Post(imagePath: "", title: "Post3", description: "Post 3 Description")
+        
+        posts.append(post)
+        posts.append(post2)
+        posts.append(post3)
+        //end test data
+        
+        tableView.reloadData()
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
