1b1a673e3e7a559f636c881a71eee6be85e5ab27
mikepitre <michaelepitre@gmail.com>
2015-11-02 12:00:02 -0600

    added outlets and actions for AddPostVC

--- DailyScenes/AddPostVC.swift
+++ DailyScenes/AddPostVC.swift
@@ -9,11 +9,26 @@
 import UIKit
 
 class AddPostVC: UIViewController {
-
+    
+    @IBOutlet weak var addSceneImage: UIImageView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
-
+    
+    @IBAction func addSceneBtnPressed(sender: UIButton) {
+        sender.setTitle("", forState: .Normal)
+    }
+    
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func addPicBtnPressed(sender: AnyObject) {
+    }
+    
 }
