b1bc8d46d6e971df0ce53343d410237c81624a1d
mikepitre <michaelepitre@gmail.com>
2015-11-02 15:19:07 -0600

    added code to move up the view when the keyboard is open

--- DailyScenes/AddPostVC.swift
+++ DailyScenes/AddPostVC.swift
@@ -16,6 +16,8 @@ class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     
     var imagePicker: UIImagePickerController!
     
+    var kbHeight: CGFloat!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +26,8 @@ class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         addSceneImage.clipsToBounds = true
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        titleField.delegate = self
+        descField.delegate = self
     }
 
     
@@ -53,5 +57,46 @@ class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        titleField.resignFirstResponder()
+        descField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewWillAppear(animated:Bool) {
+        super.viewWillAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardSize =  (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                kbHeight = keyboardSize.height
+                self.animateTextField(true)
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.animateTextField(false)
+    }
+    
+    func animateTextField(up: Bool) {
+        let movement = (up ? -kbHeight : kbHeight)
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.view.frame = CGRectOffset(self.view.frame, 0, movement)
+        })
+    }
+    
     
 }
