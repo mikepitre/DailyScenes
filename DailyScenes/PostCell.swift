aadbf343a740424d215bdbda31a7c64d4403c1ee
mikepitre <michaelepitre@gmail.com>
2015-11-02 11:05:57 -0600

    added outlets to custom cell class

---
+++ DailyScenes/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  DailyScenes
+//
+//  Created by Mike Pitre on 11/2/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
